96d30dfd0380e2bf9fb11ee744ac7903280abe0c
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-01 17:45:21 -0800

    Update models

--- app/models.py
+++ app/models.py
@@ -1,54 +1,45 @@
 from app import db
 
 
-class BaseModel(db.Model):
-    """Base data model for all objects"""
-    __abstract__ = True
-
-    def __init__(self, *args):
-        super().__init__(*args)
-
-    def __repr__(self):
-        """Define a base way to print models"""
-        return '%s(%s)' % (self.__class__.__name__, {
-            column: value
-            for column, value in self._to_dict().items()
-        })
-
-    def json(self):
-        """
-                Define a base way to jsonify models, dealing with datetime objects
-        """
-        return {
-            column: value if not isinstance(
-                value, datetime.date) else value.strftime('%Y-%m-%d')
-            for column, value in self._to_dict().items()
-        }
-
 tags = db.Table('tags',
     db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
     db.Column('entry_id', db.Integer, db.ForeignKey('entry.id'), primary_key=True)
 )
 
 class Account(db.Model):
+    __tablename__   = 'account'
+
     id              = db.Column(db.Integer, primary_key=True, nullable=False)
     created_on      = db.Column(db.Date, unique=False, nullable=False)
-    last_login      = db.Column(db.Date, unique=False, nullable=True)
-    username        = db.Column(db.String(48), index=True, unique=True, nullable=False)
-    password_hash   = db.Column(db.String(128))
     email           = db.Column(db.String(120), index=True, unique=True)
+    last_login      = db.Column(db.Date, unique=False, nullable=True)
+    password_hash   = db.Column(db.String(128))
+    username        = db.Column(db.String(48), index=True, unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Account {}>'.format(self.username)
 
 class Entry(db.Model):
+    __tablename__   = 'entry'
+
     id              = db.Column(db.Integer, primary_key=True, nullable=False)
     account_id      = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
     content         = db.Column(db.Text, unique=False, nullable=False)
     created_on      = db.Column(db.Date, unique=False, nullable=False)
-    last_modified   = db.Column(db.Date, unique=False, nullable=True)
+    modified_on     = db.Column(db.Date, unique=False, nullable=True)
     tags            = db.relationship('Tag', secondary=tags, lazy='subquery', backref=db.backref('entry', lazy=True))
     title           = db.Column(db.String(128), unique=False, nullable=False)
 
+    def __repr__(self):
+        return '<Entry {}>'.format(self.title)
+
 class Tag(db.Model):
+    __tablename__   = 'tag'
+
     id              = db.Column(db.Integer, primary_key=True, nullable=False)
     created_on      = db.Column(db.Date, unique=False, nullable=False)
     modified_on     = db.Column(db.Date, unique=False, nullable=False)
-    name            = db.Column(db.String(24), unique=True, nullable=False)
+    name            = db.Column(db.String(48), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Tag {}>'.format(self.name)
