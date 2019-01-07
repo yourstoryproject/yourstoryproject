092724a103df795e356f3095be6694291280b7ef
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-06 19:59:16 -0800

    YSP-1 Update init methods to use datetime for default values

--- app/models.py
+++ app/models.py
@@ -12,8 +12,8 @@ class Account(db.Model):
     __tablename__   = 'account'
 
     id              = db.Column(db.Integer, primary_key=True, nullable=False)
-    created_on      = db.Column(db.Date, unique=False, nullable=False)
-    email           = db.Column(db.String(120), index=True, unique=True)
+    created_on      = db.Column(db.DateTime, default=datetime.datetime.utcnow, unique=False, nullable=False)
+    email           = db.Column(db.String(120), index=True, unique=True, nullable=False)
     last_login      = db.Column(db.Date, unique=False, nullable=True)
     password_hash   = db.Column(db.String(128))
     username        = db.Column(db.String(48), index=True, unique=True, nullable=False)
@@ -21,8 +21,8 @@ class Account(db.Model):
     def __repr__(self):
         return '<Account {}>'.format(self.username)
 
-    def __init__(self, created_on, email, last_login, password, username):
-        self.created_on = created_on
+    def __init__(self, email, last_login, password, username):
+        self.created_on = datetime.datetime.utcnow()
         self.email  = email.lower()
         self.last_login  = last_login
         self.set_password(password)
@@ -40,18 +40,18 @@ class Entry(db.Model):
     id              = db.Column(db.Integer, primary_key=True, nullable=False)
     account_id      = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
     content         = db.Column(db.Text, unique=False, nullable=False)
-    created_on      = db.Column(db.Date, unique=False, nullable=False)
-    modified_on     = db.Column(db.Date, unique=False, nullable=True)
+    created_on      = db.Column(db.DateTime, default=datetime.datetime.utcnow, unique=False, nullable=False)
+    modified_on     = db.Column(db.DateTime, unique=False, nullable=True)
     tags            = db.relationship('Tag', secondary=tags, lazy='subquery', backref=db.backref('entry', lazy=True))
     title           = db.Column(db.String(128), unique=False, nullable=False)
 
     def __repr__(self):
         return '<Entry {}>'.format(self.title)
 
-    def __init__(self, account_id, content, created_on, modified_on, tags, title):
+    def __init__(self, account_id, content, modified_on, tags, title):
         self.account_id = account_id
         self.content = content
-        self.created_on = created_on
+        self.created_on = datetime.datetime.utcnow()
         self.modified_on = modified_on
         self.tags = tags
         self.title = title
@@ -60,14 +60,14 @@ class Tag(db.Model):
     __tablename__   = 'tag'
 
     id              = db.Column(db.Integer, primary_key=True, nullable=False)
-    created_on      = db.Column(db.Date, unique=False, nullable=False)
-    modified_on     = db.Column(db.Date, unique=False, nullable=False)
+    created_on      = db.Column(db.DateTime, default=datetime.datetime.utcnow, unique=False, nullable=False)
+    modified_on     = db.Column(db.Date, unique=False, nullable=True)
     name            = db.Column(db.String(48), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Tag {}>'.format(self.name)
 
-    def __init__(self, created_on, modified_on, name):
-        self.created_on = created_on
+    def __init__(self, modified_on, name):
+        self.created_on = datetime.datetime.utcnow()
         self.modified_on = modified_on
         self.name = name.lower()
