5209cfaab035f35cee445673a8d356b594f405cb
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-06 20:14:06 -0800

    YSP-8 Add shell context

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from app import app
 from app import db
+from app.models import Account, Entry, Tag
 
 db.init_app(app)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Account': Account, 'Entry': Entry, 'Tag': Tag}
