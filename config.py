a6864b7a9546e4cf2775613824325ff50aed4e4c
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-06 18:42:25 -0800

    YSP-1 Update configs to set up config options for prod, staging, dev, and testing

--- config.py
+++ config.py
@@ -5,7 +5,26 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = os.environ.get('DEBUG') or False
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'some-pass'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
