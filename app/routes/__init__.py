8c800f9d24d9a43a6f1c58e2d84a4579f54e8ac0
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-07 18:08:58 -0800

    YSP-5 Register blueprint to routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,9 +1,10 @@
-from app.routes import Tag
+from app.routes import Account, Tag
 from app import app
 from flask import render_template
 
 
 app.register_blueprint(Tag.blueprint)
+app.register_blueprint(Account.blueprint)
 
 @app.route('/')
 def index():
