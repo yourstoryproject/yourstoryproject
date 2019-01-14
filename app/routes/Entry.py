cf9e3dc7373cde9e7d07a78eb5e6a8e37fedf9a8
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-13 21:22:38 -0800

    YSP-20 Update get entry routes

--- app/routes/Entry.py
+++ app/routes/Entry.py
@@ -1,19 +1,25 @@
 from app.utils.server import parse_response
 from app.models import Entry
 from app.api.Entry import get_entry, get_entries
-from flask import Blueprint, jsonify
-
+from flask import Blueprint, jsonify, render_template, request
 
 blueprint = Blueprint('entries', __name__, url_prefix='/api/v1/entries')
 
 @blueprint.route('/', methods=['GET'])
-def entries():
+def index():
+    return render_template('api.html')
+
+
+@blueprint.route('/get_entries/', methods=['GET'])
+def getAllEntries():
     response = get_entries()
 
-    return parse_response(response, response["status_code"])
+    return render_template('api.html', response=response)
 
-@blueprint.route('/<int:entryId>', methods=['GET'])
-def entry(entryId):
+@blueprint.route('/get_entry/', methods=['GET'])
+def getSingleEntry():
+    entryId = request.args.get('entryId')
+
     response = get_entry(entryId)
 
-    return parse_response(response, response["status_code"])
+    return render_template('api.html', response=response)
