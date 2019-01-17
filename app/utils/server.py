c80a1b612f4ede46ef338cd2657f1bfd67d8666e
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-16 21:18:16 -0800

    YSP-24 Add validate entity util to check for entity

--- app/utils/server.py
+++ app/utils/server.py
@@ -10,3 +10,23 @@ def parse_response(res, status_code):
         response=json.dumps(res),
         status=status_code
     )
+
+
+def validate_entity(model, entityId):
+    """
+    Check if <model> has a matching entity by <entityId>
+    """
+    if entityId == '':
+        response = {"error": "Please provide entityId", "status_code": 400}
+
+        return response
+
+    if not model.query.filter_by(id=entityId).first():
+        response = {
+            "error": "No entity found with entityId: " +
+            entityId,
+            "status_code": 400}
+
+        return response
+    else:
+        return None
