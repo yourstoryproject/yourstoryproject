92d9ec19f0e03d261c7046810a5b844013f08221
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-03-25 14:01:59 -0700

    YSP-40 SF

--- manage.py
+++ manage.py
@@ -4,14 +4,12 @@ from flask_migrate import Migrate, MigrateCommand
 
 from pyapp import pyapp, db
 
-
 pyapp.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(pyapp, db)
 manager = Manager(pyapp)
 
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
